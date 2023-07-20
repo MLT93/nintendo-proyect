c3bbdf73102ac9e5413cf09d1d3160dfb7622453
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-07-20 19:46:22 +0200

    feat(section-nav-search-dropdown-button.js): add script for search button

---
+++ section-nav-search-dropdown-button.js
@@ -0,0 +1,17 @@
+// /* función que me permite navegar por el dropdown del NavBar */
+function nav(value) {
+    if (value !== "") {
+      location.href = value;
+    }
+    return location.href;
+  }
+  
+// /* función para mostrar u ocultar información tramite un botón */
+  function myFunction() {
+  let elementId = document.getElementById("src-dropdown_svg-path");
+  if (elementId.style.display === "none") {
+    elementId.style.display = "block";
+  } else {
+    elementId.style.display = "none";
+  }
+}
