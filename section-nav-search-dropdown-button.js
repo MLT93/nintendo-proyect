6509f84e712e9dd981b1548a603a7dffb5f9c207
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-07-31 23:11:58 +0200

    feat: acabo el dropdown-button pero con un problema

--- section-nav-search-dropdown-button.js
+++ section-nav-search-dropdown-button.js
@@ -1,17 +1,89 @@
 /* función que me permite navegar por el dropdown del NavBar */
 function nav(value) {
-    if (value !== "") {
-      location.href = value;
-    }
-    return location.href;
+  if (value !== "") {
+    location.href = value;
   }
-  
+  return location.href;
+}
+
 /* función para mostrar u ocultar información tramite un botón */
-  function myFunction() {
+function myFunction() {
   let elementId = document.getElementById("src-dropdown_svg-path");
-  if (elementId.style.display === "none") {
-    elementId.style.display = "block";
-  } else {
+  if (elementId.style.display === "block") {
     elementId.style.display = "none";
+  } else {
+    elementId.style.display = "block";
   }
 }
+/* myFunction(); */
+
+/* función para mostrar u ocultar zonas a través de botones, agregando o quitando clases (.toggle) según la necesidad */
+/* window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM completamente cargado y procesado");
+
+  const menuBtnAbrir = document.getElementById("menu-abrir");
+  const menuBtnCerrar = document.getElementById("menu-cerrar");
+  const hTynpQ = document.getElementById("menu-search-dropdown");
+  const headerRight = document.querySelector("div.header-right"); */
+
+/*menuBtnAbrir.addEventListener("click", (event) => {
+    if (menuBtnAbrir.classList.toggle("open")) {
+      hTynpQ.classList.toggle("visible"); */
+/* hTynpQ.style.display = "block"; */
+/*  headerRight.classList.toggle("no-visible"); */
+/* headerRight.style.display = "none"; */
+/*   menuBtnCerrar.classList.toggle("visible"); */
+/* menuBtnCerrar.style.display = "block"; */
+/*     }
+  }); */
+
+/*  menuBtnCerrar.addEventListener("click", (event) => {
+    if (menuBtnCerrar.classList.toggle("close")) {
+      hTynpQ.classList.toggle("no-visible"); */
+/* hTynpQ.style.display = "none"; */
+/*  headerRight.classList.toggle("visible"); */
+/* headerRight.style.display = "block"; */
+/*   }
+  });
+}); */
+
+/* Otra opción: */
+
+/* Esperamos a que se cargue el DOM antes de asignar eventos */
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM completamente cargado y procesado");
+  /* Obtenemos referencias a los elementos del DOM */
+  const menuBtnAbrir = document.getElementById("menu-abrir");
+  const menuBtnCerrar = document.getElementById("menu-cerrar");
+  const hTynpQ = document.getElementById("menu-search-dropdown");
+  const headerRight = document.querySelector("div.header-right");
+  /* Función para abrir el menú. Añadir onclick="abrirMenu()" para que el botón utilice la función */
+  function abrirMenu() {
+    menuBtnAbrir.classList.toggle("open");
+    menuBtnCerrar.classList.toggle("visible");
+    hTynpQ.classList.toggle("visible");
+    headerRight.classList.toggle("no-visible");
+  }
+  /* Función para cerrar el menú. Añadir onclick="cerrarMenu()" para que el botón utilice la función */
+  function cerrarMenu() {
+    menuBtnAbrir.classList.toggle("open");
+    menuBtnCerrar.classList.toggle("visible");
+    hTynpQ.classList.toggle("visible");
+    headerRight.classList.toggle("no-visible");
+  }
+  /* Asignamos eventos a los botones */
+  menuBtnAbrir.addEventListener("click", abrirMenu);
+  menuBtnCerrar.addEventListener("click", cerrarMenu);
+
+  // Agregamos un listener a todos los enlaces de tendencias
+  const linksTendencias = document.querySelectorAll(".hTynpQ .letters");
+  linksTendencias.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      // Evitamos que el enlace redirija
+      event.preventDefault();
+      // Mantenemos el menú abierto
+      abrirMenu();
+      // Realizamos cualquier otra acción que desees cuando se hace clic en un enlace de tendencia
+    });
+  });
+});
